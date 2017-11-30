7a4a71acb7940d79ed788cf34d312a54d3d79c45
wtlin1228 <wtlin0711@gmail.com>
2017-12-01 00:10:31 +0800

    add AsyncStorage

--- components/AddEntry.js
+++ components/AddEntry.js
@@ -6,6 +6,7 @@ import MyStepper from './MyStepper'
 import DateHeader from './DateHeader'
 import TextButton from './TextButton'
 import { Ionicons } from '@expo/vector-icons'
+import { submitEntry, removeEntry } from '../utils/api'
 
 function SubmitBtn ({ onPress }) {
 	return (
@@ -71,13 +72,15 @@ export default class AddEntry extends Component {
 
 		// Navigate to home
 
-		// Save to 'DB'
+		submitEntry({ key, entry })
 
 		// Clear local notification
 	}
 
 	reset = () => {
-	  const key = timeToString()
+		const key = timeToString()
+		
+		removeEntry(key)
   }
 
   render() {
